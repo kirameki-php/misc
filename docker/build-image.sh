0568cc34a956a6f8034436abf86f94d4526154fe
Taka Oyama <toyama4649@hotmail.com>
2025-01-08 00:42:56 +0900

    build multiplatform
    # Conflicts:
    #	.idea/workspace.xml

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -6,8 +6,11 @@ today=$(date +'%Y-%m-%d')
 
 docker login
 
+# docker buildx create --name mybuilder --use
+# docker buildx install
+
 docker buildx build . \
-  --platform linux/amd64,linux/arm64 \
+  --platform linux/arm64,linux/amd64 \
   --push \
   --tag toyama4649/kirameki-php-cli:latest \
   --tag toyama4649/kirameki-php-cli:"${today}"
